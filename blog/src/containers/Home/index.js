6a15efe99287599e9811884a3bc4552f974c222c
SidAngular1996 <62620333+SidAngular1996@users.noreply.github.com>
2020-08-09 13:54:43 +0530

    Starting with navbar

---
+++ blog/src/containers/Home/index.js
@@ -0,0 +1,15 @@
+import React from  'react'
+import './style.css'
+import Header from '../../components/Headers'
+ 
+
+
+const Home = props  => {
+    return (
+        <div>
+     
+        </div>
+    )
+}
+
+export default Home; 
